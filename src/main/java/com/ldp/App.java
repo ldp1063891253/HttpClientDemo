15e4b61fbb24faae4a4a2cd466a10aefef025a73
ldp <15525743169@163.com>
2021-01-31 15:57:38 +0800

    第2次提交

--- src/main/java/com/ldp/App.java
+++ src/main/java/com/ldp/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         try {
-            HttpClientResult httpClientResult = HttpClientUtils.doGet("https://study.gupaoedu.cn/home");
+            HttpClientResult httpClientResult = HttpClientUtils.doGet("https://uri.amap.com/marker?position=116.473195,39.993253");
             System.out.println(httpClientResult.getContent());
         } catch (Exception e) {
             e.printStackTrace();
